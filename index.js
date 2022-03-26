7bbe6724c8d7b52a9e4039e7cd418e9971d079f9
Saref111 <saref012@gmail.com>
2022-03-26 16:08:40 +0200

    got context

--- index.js
+++ index.js
@@ -1,42 +1,58 @@
+import fs from "fs"
+import path from "path"
+
 const alias8n = function(config) {
-    var name = path.basename;
-    var nameArr = name.toLowerCase().split("")
-    var newName = nameArr.map((it) => {
-      if (Object.keys(ABC_MAP).some(key => key === it)) {
-        return ABC_MAP[it];
-      } else {
-        return it
-      }
-    }).join("");
+    switch (false) {
+        case !!config.ctxPath: 
+            throw Error("No context path found")
+        case fs.existsSync(path.resolve(config.ctxPath)):
+            throw Error("No context file found by given path")
+    }
+
+    const ctx = JSON.parse(fs.readFileSync(config.ctxPath))
+
+    // var name = path.basename;
+    // var nameArr = name.toLowerCase().split("")
+    // var newName = nameArr.map((it) => {
+    //   if (Object.keys(ABC_MAP).some(key => key === it)) {
+    //     return ABC_MAP[it];
+    //   } else {
+    //     return it
+    //   }
+    // }).join("");
 
 
-    var htmlFolder = fs.readdirSync('./source/scss/blocks/')
-    htmlFolder.forEach((file) => {
-      if (file.endsWith('.scss')) {
-          var a = fs.readFileSync('./source/scss/blocks/' + file)
+    // var htmlFolder = fs.readdirSync('./source/scss/blocks/')
+    // htmlFolder.forEach((file) => {
+    //   if (file.endsWith('.scss')) {
+    //       var a = fs.readFileSync('./source/scss/blocks/' + file)
 
-          var r = new RegExp(name, 'g')
+    //       var r = new RegExp(name, 'g')
 
-          a = a.toString().replace(r, newName)
+    //       a = a.toString().replace(r, newName)
 
-          fs.writeFileSync('./source/scss/blocks/' + file, a)
-      }
-    } )
+    //       fs.writeFileSync('./source/scss/blocks/' + file, a)
+    //   }
+    // } )
 
-    htmlFolder = fs.readdirSync('./source/')
-    htmlFolder.forEach((file) => {
-      if (file.endsWith('.html')) {
-          var a = fs.readFileSync('./source/' + file)
+    // htmlFolder = fs.readdirSync('./source/')
+    // htmlFolder.forEach((file) => {
+    //   if (file.endsWith('.html')) {
+    //       var a = fs.readFileSync('./source/' + file)
 
-          var r = new RegExp(name, 'g')
+    //       var r = new RegExp(name, 'g')
 
-          a = a.toString().replace(r, newName)
+    //       a = a.toString().replace(r, newName)
 
-          fs.writeFileSync('./source/' + file, a)
-      }
-    } )
+    //       fs.writeFileSync('./source/' + file, a)
+    //   }
+    // } )
     
-    return path.basename = newName;
-  }
+    // return path.basename = newName;
+}
 
-alias8n()
+alias8n({
+    ctxPath: "test/test.json",
+    source: "./test/test.html",
+    dest: "./test/",
+})
