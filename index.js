a01a5d924576e18a1e66b0c3594c39323bd59907
Saref111 <saref012@gmail.com>
2022-03-27 12:10:45 +0300

    added export

--- index.js
+++ index.js
@@ -4,30 +4,35 @@ const alias8n = function(config) {
     const {
         ctxPath = "./ctx.json",
         source = "./index.html",
-        dest = "./index-aliased.html"
+        dest = "./index-aliased.html",
+        aliasMarker = "a\\(:*:\\)", // use * for alias name
     } = config
 
     switch (false) {
-        case fs.existsSync(config.ctxPath):
+        case fs.existsSync(ctxPath):
             throw Error("No context file found by given path")
-        case fs.existsSync(config.source):
+        case ctxPath.endsWith(".json"):
+            throw Error("Context file should be a json file")
+        case fs.existsSync(source):
             throw Error("No source file found by given path")
     }
 
-    const ctx = JSON.parse(fs.readFileSync(config.ctxPath))
+    const ctx = JSON.parse(fs.readFileSync(ctxPath))
     
-    let srcFile = fs.readFileSync(config.source).toString()
+    let srcFile = fs.readFileSync(source).toString()
 
     for (let alias in ctx) {
-        const aliasR = new RegExp("a\\(:" + alias.trim() + ":\\)", 'g')
-        srcFile = srcFile.replace(aliasR, ctx[alias])
+        const aliasMarkerRegex = new RegExp(aliasMarker.replace(/\*/g, alias), "g")
+        srcFile = srcFile.replace(aliasMarkerRegex, ctx[alias])
     }
 
-    fs.writeFileSync(config.dest, srcFile)
+    fs.writeFileSync(dest, srcFile)
 }
 
-alias8n({
-    ctxPath: "test/test.json",
-    source: "./test/test.html",
-    dest: "./test/test-al.html",
-})
+// alias8n({
+//     ctxPath: "test/test.json",
+//     source: "./test/test.html",
+//     dest: "./test/test-al.html",
+// })
+
+export default alias8n
