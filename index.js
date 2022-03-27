a19d40108889f62500f78afa42f469beed27cddc
Saref111 <saref012@gmail.com>
2022-03-27 11:33:32 +0300

    created main functionality

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import fs from "fs"
-import path from "path"
 
 const alias8n = function(config) {
+    const {
+        ctxPath = "./ctx.json",
+        source = "./index.html",
+        dest = "./index-aliased.html"
+    } = config
+
     switch (false) {
         case fs.existsSync(config.ctxPath):
             throw Error("No context file found by given path")
@@ -14,62 +19,15 @@ const alias8n = function(config) {
     let srcFile = fs.readFileSync(config.source).toString()
 
     for (let alias in ctx) {
-        console.log(alias);
-        const aliasR = new RegExp(alias.trim(), 'g')
-        console.log(srcFile);
+        const aliasR = new RegExp("a\\(:" + alias.trim() + ":\\)", 'g')
         srcFile = srcFile.replace(aliasR, ctx[alias])
-        console.log(srcFile);
-        // console.log(newFileString);
     }
 
-
-
-    //       a = a.toString().replace(r, newName)
-
-    //       fs.writeFileSync('./source/scss/blocks/' + file, a)
-
-    // var name = path.basename;
-    // var nameArr = name.toLowerCase().split("")
-    // var newName = nameArr.map((it) => {
-    //   if (Object.keys(ABC_MAP).some(key => key === it)) {
-    //     return ABC_MAP[it];
-    //   } else {
-    //     return it
-    //   }
-    // }).join("");
-
-
-    // var htmlFolder = fs.readdirSync('./source/scss/blocks/')
-    // htmlFolder.forEach((file) => {
-    //   if (file.endsWith('.scss')) {
-    //       var a = fs.readFileSync('./source/scss/blocks/' + file)
-
-    //       var r = new RegExp(name, 'g')
-
-    //       a = a.toString().replace(r, newName)
-
-    //       fs.writeFileSync('./source/scss/blocks/' + file, a)
-    //   }
-    // } )
-
-    // htmlFolder = fs.readdirSync('./source/')
-    // htmlFolder.forEach((file) => {
-    //   if (file.endsWith('.html')) {
-    //       var a = fs.readFileSync('./source/' + file)
-
-    //       var r = new RegExp(name, 'g')
-
-    //       a = a.toString().replace(r, newName)
-
-    //       fs.writeFileSync('./source/' + file, a)
-    //   }
-    // } )
-    
-    // return path.basename = newName;
+    fs.writeFileSync(config.dest, srcFile)
 }
 
 alias8n({
     ctxPath: "test/test.json",
     source: "./test/test.html",
-    dest: "./test/",
+    dest: "./test/test-al.html",
 })
